6ac8d5fdbb0596671c93a2d4aee14d6631b36030
Renderix <ayusman.c@udaan.com>
2025-08-03 14:56:02 +0530

    remove BookingResource binding from BookingModule

--- flight-booking-service/src/main/kotlin/com/credair/booking/config/BookingModule.kt
+++ flight-booking-service/src/main/kotlin/com/credair/booking/config/BookingModule.kt
@@ -1,14 +1,11 @@
 package com.credair.booking.config
 
-import com.credair.booking.resource.BookingResource
 import com.credair.core.config.CredAirCoreModule
 import com.google.inject.AbstractModule
-import com.google.inject.Singleton
 
 class BookingModule : AbstractModule() {
 
     override fun configure() {
         install(CredAirCoreModule())
-        bind(BookingResource::class.java).`in`(Singleton::class.java)
     }
 }
