1f9ee516f7c2264b0dbd105fd2b353ff5372c12f
Renderix <ayusman.c@udaan.com>
2025-08-03 12:30:47 +0530

    add data classes for booking requests and passenger information

---
+++ cred-air-core/src/main/kotlin/com/credair/core/model/booking-dto.kt
@@ -0,0 +1,37 @@
+package com.credair.core.model
+
+import java.math.BigDecimal
+
+data class BookingRequest(
+    val flightId: Long,
+    val passengerName: String,
+    val passengerEmail: String,
+    val passengerPhone: String? = null,
+    val numberOfSeats: Int = 1,
+    val specialRequests: String? = null,
+    val paymentMethod: String? = null
+)
+
+data class BookingRequestPayload(
+    val flightIds: List<String>,
+    val passengerData: List<PassengerData>,
+    val flightPrices: List<FlightPricePayload>,
+    val totalPrice: BigDecimal,
+    val passengerCount: Int
+)
+
+data class FlightPricePayload(
+    val flightId: String,
+    val price: BigDecimal,
+    val currency: String
+)
+
+data class PassengerData(
+    val id: String,
+    val title: String,
+    val firstName: String,
+    val lastName: String,
+    val dateOfBirth: String,
+    val email: String,
+    val phone: String
+)
